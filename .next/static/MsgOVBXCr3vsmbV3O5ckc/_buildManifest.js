a566b99158b52df2795489b3e4a842a97f89a04e
TakamiInagaki <takami.l.inagaki@gmail.com>
2023-02-27 10:25:26 -0800

    update

---
+++ .next/static/MsgOVBXCr3vsmbV3O5ckc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/3bc454771c0c000a.css","static/chunks/pages/index-9e184f97f74920db.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
